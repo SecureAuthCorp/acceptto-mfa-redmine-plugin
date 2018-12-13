7a8d26bc510e4b4fecf37b0ae79a426f4e45728f
Mani Malekmohammadi <mm.mani@gmail.com>
2018-12-12 16:23:27 -0800

    change default endpoint to mfa.acceptto.com

--- acceptto_mfa/init.rb
+++ acceptto_mfa/init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :acceptto_mfa do
 
   require_dependency 'mfa_hook_listener'
 
-  settings :default => {'mfa_site' => 'https://www.acceptto.com', 'auth_message' => 'Do you authorize redmine login?', 'app_uid' => '82441b628e122e6d7137d2ba94a8de79973ba6da333f9d68f8ec3ab7bf1b3b6d', 'app_secret' => 'c00be1fd3696c537956d8b4a39ae106ed5df12cbb8396c9edade7f7c36b1343a', 'auth_timeout' => '60'}, :partial => 'settings/acceptto_mfa_settings'
+  settings :default => {'mfa_site' => 'https://mfa.acceptto.com', 'auth_message' => 'Do you authorize redmine login?', 'app_uid' => '82441b628e122e6d7137d2ba94a8de79973ba6da333f9d68f8ec3ab7bf1b3b6d', 'app_secret' => 'c00be1fd3696c537956d8b4a39ae106ed5df12cbb8396c9edade7f7c36b1343a', 'auth_timeout' => '60'}, :partial => 'settings/acceptto_mfa_settings'
 
   Rails.configuration.to_prepare do
     ApplicationController.send(:include, ApplicationControllerPatch)
